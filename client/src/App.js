36ea50e08357396567e9effd312267d76875b5a6
Suraj <suraj.chand@vedastudios.com>
2023-06-01 00:42:27 +0545

    feat: setup socket.io on backend and verify on client

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import PostComponent from "./Components/Product/ProductComponent";
 import PostDetailsComponent from "./Components/Product/ProductDetailComponent";
 
+const socket = require("socket.io-client")("http://localhost:4001");
+
+socket.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 function App() {
   return (
     <>
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
+const { Server } = require("socket.io");
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("disconnect", (reason) => {
+    console.log("Client disconnected:", socket.id, "Reason:", reason);
+  });
+});
+
 server.listen(PORT, () => {
-  console.info(`Server is start on http://localhost:${PORT}`);
+  console.info(`Server is running on http://localhost:${PORT}`);
 });
