27c016ccab61a3a3213c2550fab10eb83893c54c
Suraj <surajchan68@gmail.com>
2023-06-01 23:42:00 +0545

    feat: implement realtime error

--- client/src/Components/Product/ProductDetailComponent.js
+++ client/src/Components/Product/ProductDetailComponent.js
@@ -1,28 +1,49 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import AddComment from "../Comment/AddCommentComponent";
+// import AddComment from "../Comment/AddCommentComponent";
 import { socket } from "../../App";
 
 function PostDetailsComponent() {
   const { id } = useParams();
 
-  const [data, setData] = useState("");
   const API_URL = process.env.REACT_APP_API_URL + "/post/" + id;
-  const [post, setPost] = useState([]);
+  const [post, setPost] = useState({});
+  const [comment, setComment] = useState({ content: "" });
+  const handleChange = (event) => {
+    setComment({ content: event.target.value });
+  };
+
+  const postComment = () => {
+    // axios
+    //   .post(API_URL, {
+    //     ...comment,
+    //     user_id: "55b55519-3d06-4fe8-a9ea-cac63474cf57",
+    //     // user_id: "55b55519-3d06-4fe8-a9ea-cac63474cf58",
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+    socket.emit(socket.id, {
+      postId: post.id,
+      ...comment,
+      user_id: "55b55519-3d06-4fe8-a9ea-cac63474cf57",
+    });
+
+    socket.on(post.id, (comment) => {
+      post.Comment.push(comment);
+      setPost({ ...post });
+    });
+  };
 
   useEffect(() => {
     axios.get(API_URL).then(({ data }) => {
-      socket.on("FromAPI", (data) => {
-        setData(data);
-      });
       setPost(data.data);
     });
   }, []);
 
   return (
     <div>
-      <p>{data}</p>
       <section style={{ backgroundColor: " #eee" }}>
         <div className="container my-5 py-5">
           <div className="row d-flex justify-content-center">
@@ -61,7 +82,34 @@ function PostDetailsComponent() {
                       </div>
                     );
                   })}
-                <AddComment postId={post.id} />
+                {/* <AddComment postId={post.id} /> */}
+                <div
+                  className="card-footer py-3 border-0"
+                  style={{ backgroundColor: "#f8f9fa" }}
+                >
+                  <label className="form-label">Comment</label>
+                  <div className="d-flex flex-start w-100">
+                    <div className="form-outline w-100">
+                      <textarea
+                        className="form-control"
+                        id="textAreaExample"
+                        rows="4"
+                        style={{ background: "#fff" }}
+                        onChange={handleChange}
+                        value={comment.content}
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="float-end mt-2 pt-1">
+                    <button
+                      type="button"
+                      onClick={postComment}
+                      className="btn btn-primary btn-sm"
+                    >
+                      Comment
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -36,8 +36,43 @@ exports.addComment = async (req, res) => {
 exports.addCommentWithSocket = (io) => {
   io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
-    socket.emit("FromAPI", "hello");
 
+    socket.on(socket.id, async (data) => {
+      const post = await prisma.post.findUnique({ where: { id: data.postId } });
+      // if (!post) {
+      //   return res.status(404).json({
+      //     success: false,
+      //     message: "Post record not found",
+      //   });
+      // }
+
+      comment = await prisma.comment.create({
+        data: {
+          content: data.content,
+          post_id: post.id,
+          user_id: data.user_id,
+        },
+      });
+
+      comment = await prisma.comment.findUnique({
+        where: { id: comment.id },
+        include: { User: true },
+      });
+      socket.emit(post.id, comment);
+
+      // if (!comment) {
+      //   return res.status(400).json({
+      //     success: false,
+      //     message: "Unable to comment",
+      //   });
+      // }
+
+      // return res.status(200).json({
+      //   success: true,
+      //   message: "Comment added successfully",
+      //   data: comment,
+      // });
+    });
     socket.on("disconnect", (reason) => {
       console.log("Client disconnected:", socket.id, "Reason:", reason);
     });
